e5206a85b7b9d0273013972691420f5a8901016a
Lu <603769201@qq.com>
2017-02-17 09:46:43 +0800

    model test

--- ecgDataProcess.lua
+++ ecgDataProcess.lua
@@ -1,34 +1,71 @@
 require 'torch'
 require 'hdf5'
 
-N, T = 1, 1
+local utils = require 'util.utils'
 
--- Just slurp all the data into memory
-splits = {}
-f = hdf5.open('/home/lu/torch-rnn/my_data.h5', 'r')
+local DataLoader = torch.class('DataLoader')
 
-splits.train = f:read('/train'):all()
-splits.val = f:read('/val'):all()
-splits.test = f:read('/test'):all()
 
-data.x_splits = {}
-data.y_splits = {}
-data.split_sizes = {}
+function DataLoader:__init(kwargs)
+  
+  trainData = torch.load('RnnTrain.t7')
+  testData = torch.load('RnnTest.t7')
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  local h5_file = utils.get_kwarg(kwargs, 'input_h5')
+  self.batch_size = utils.get_kwarg(kwargs, 'batch_size')
+  self.seq_length = utils.get_kwarg(kwargs, 'seq_length')
+  local N, T = self.batch_size, self.seq_length
 
-for split, v in pairs(splits) do
-	local num = v:nElement()
-	local extra = num % (N * T)
+  -- Just slurp all the data into memory
+  local splits = {}
+  local f = hdf5.open(h5_file, 'r')
+  splits.train = f:read('/train'):all()
+  splits.val = f:read('/val'):all()
+  splits.test = f:read('/test'):all()
 
-	-- Ensure that `vy` is non-empty
-	if extra == 0 then
-	  extra = N * T
-	end
+  self.x_splits = {}
+  self.y_splits = {}
+  self.split_sizes = {}
+  for split, v in pairs(splits) do
+    local num = v:nElement()
+    local extra = num % (N * T)
 
-	-- Chop out the extra bits at the end to make it evenly divide
-	local vx = v[{{1, num - extra}}]:view(N, -1, T):transpose(1, 2):clone()
-	local vy = v[{{2, num - extra + 1}}]:view(N, -1, T):transpose(1, 2):clone()
-	
-	data.x_splits[split] = vx
-	data.y_splits[split] = vy
-	data.split_sizes[split] = vx:size(1)
+    -- Ensure that `vy` is non-empty
+    if extra == 0 then
+      extra = N * T
+    end
+
+    -- Chop out the extra bits at the end to make it evenly divide
+    local vx = v[{{1, num - extra}}]:view(N, -1, T):transpose(1, 2):clone()
+    local vy = v[{{2, num - extra + 1}}]:view(N, -1, T):transpose(1, 2):clone()
+
+    self.x_splits[split] = vx
+    self.y_splits[split] = vy
+    self.split_sizes[split] = vx:size(1)
+  end
+
+  self.split_idxs = {train=1, val=1, test=1}
 end
+
+
+function DataLoader:nextBatch(split)
+  local idx = self.split_idxs[split]
+  assert(idx, 'invalid split ' .. split)
+  local x = self.x_splits[split][idx]
+  local y = self.y_splits[split][idx]
+  if idx == self.split_sizes[split] then
+    self.split_idxs[split] = 1
+  else
+    self.split_idxs[split] = idx + 1
+  end
+  return x, y
+end
+
