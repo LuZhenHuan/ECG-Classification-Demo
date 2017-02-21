69c437cbd85ae99974aefe6a2581d236b2a06122
LuZhenHuan <603769201@qq.com>
2017-02-21 17:01:57 +0800

    update by linux

--- ecgRnnTrain.lua
+++ ecgRnnTrain.lua
@@ -83,66 +83,173 @@ local params, grad_params = model:getParameters()
 local crit = nn.CrossEntropyCriterion():type(dtype)
 
 
---Set up some variables we will use below
+-- Set up some variables we will use below
+local N, T = opt.batch_size, opt.seq_length
+local train_loss_history = {}
+local val_loss_history = {}
+local val_loss_history_it = {}
+local forward_backward_times = {}
+local init_memory_usage, memory_usage = nil, {}
 
-N, T = 1, 100		--opt.batch_size, opt.seq_length	
-count = 1
-flag  = 1
-epoch = 1
-max_epochs = 1 
-train_loss_history = {}
-
-params, grad_params = model:getParameters()
-
-trainset = torch.load('RnnTrain.t7')
---load and process data
-function data_process(dataset)
-
-	
-	xTemp = dataset[count]:view(4,-1,T)
-	count = count + 1
-
-	if count <= 50250 then
-		y = torch.Tensor{0}:view(1,1,1)
-	else
-		y = torch.Tensor{1}:view(1,1,1)
-	end
-	
-	if count == 110501 then
-		count = 1
-	end
-end
-data_process(trainset)
-
---load a batch
-function next_batch()
-	
-	x = xTemp[flag]:view(1,-1,100)
-	flag = flag + 1
-	if flag == 4 then
-		flag = 1
-	end
-	x, y = x:type(dtype), y:type(dtype)
-
-	return x,y
+if opt.memory_benchmark == 1 then
+  -- This should only be enabled in GPU mode
+  assert(cutorch)
+  cutorch.synchronize()
+  local free, total = cutorch.getMemoryUsage(cutorch.getDevice())
+  init_memory_usage = total - free
 end
 
-
---loss function 
+-- Loss function that we pass to an optim method
 local function f(w)
-  
   assert(w == params)
   grad_params:zero()
-  --data_process()
-  x,y = next_batch()
-    
+  -- Get a minibatch and run the model forward, maybe timing it
+  local timer
+  local x, y = loader:nextBatch('train')
+  x, y = x:type(dtype), y:type(dtype)
+  if opt.speed_benchmark == 1 then
+    if cutorch then cutorch.synchronize() end
+    timer = torch.Timer()
+  end
   local scores = model:forward(x)
-  local loss = crit:forward(scores, y)
-  local grad_scores = crit:backward(scores, y)
+  
+  -- Use the Criterion to compute loss; we need to reshape the scores to be
+  -- two-dimensional before doing so. Annoying.
+  local scores_view = scores:view(N * T, -1)
+  local y_view = y:view(N * T)
+  local loss = crit:forward(scores_view, y_view)  
+ 
+  -- Run the Criterion and model backward to compute gradients, maybe timing it
+  local grad_scores = crit:backward(scores_view, y_view):view(N, T, -1)
   model:backward(x, grad_scores)
-  --model:resetStates()
+  if timer then
+    if cutorch then cutorch.synchronize() end
+    local time = timer:time().real
+    print('Forward / Backward pass took ', time)
+    table.insert(forward_backward_times, time)
+  end
+
+  -- Maybe record memory usage
+  if opt.memory_benchmark == 1 then
+    assert(cutorch)
+    if cutorch then cutorch.synchronize() end
+    local free, total = cutorch.getMemoryUsage(cutorch.getDevice())
+    local memory_used = total - free - init_memory_usage
+    local memory_used_mb = memory_used / 1024 / 1024
+    print(string.format('Using %dMB of memory', memory_used_mb))
+    table.insert(memory_usage, memory_used)
+  end
+
+  if opt.grad_clip > 0 then
+    grad_params:clamp(-opt.grad_clip, opt.grad_clip)
+  end
+
   return loss, grad_params
 end
+
+-- Train the model!
+local optim_config = {learningRate = opt.learning_rate}
+local num_train = loader.split_sizes['train']
+local num_iterations = opt.max_epochs * num_train
+model:training()
+for i = start_i + 1, num_iterations do
+  local epoch = math.floor(i / num_train) + 1
+
+  -- Check if we are at the end of an epoch
+  if i % num_train == 0 then
+    model:resetStates() -- Reset hidden states
+
+    -- Maybe decay learning rate
+    if epoch % opt.lr_decay_every == 0 then
+      local old_lr = optim_config.learningRate
+      optim_config = {learningRate = old_lr * opt.lr_decay_factor}
+    end
+  end
+
+  -- Take a gradient step and maybe print
+  -- Note that adam returns a singleton array of losses
+  local _, loss = optim.adam(f, params, optim_config)
+  table.insert(train_loss_history, loss[1])
+  if opt.print_every > 0 and i % opt.print_every == 0 then
+    local float_epoch = i / num_train + 1
+    local msg = 'Epoch %.2f / %d, i = %d / %d, loss = %f'
+    local args = {msg, float_epoch, opt.max_epochs, i, num_iterations, loss[1]}
+    print(string.format(unpack(args)))
+  end
+
+  -- Maybe save a checkpoint
+  local check_every = opt.checkpoint_every
+  if (check_every > 0 and i % check_every == 0) or i == num_iterations then
+    -- Evaluate loss on the validation set. Note that we reset the state of
+    -- the model; this might happen in the middle of an epoch, but that
+    -- shouldn't cause too much trouble.
+    model:evaluate()
+    model:resetStates()
+    local num_val = loader.split_sizes['val']
+    local val_loss = 0
+    for j = 1, num_val do
+      local xv, yv = loader:nextBatch('val')
+      xv = xv:type(dtype)
+      yv = yv:type(dtype):view(N * T)
+      local scores = model:forward(xv):view(N * T, -1)
+      val_loss = val_loss + crit:forward(scores, yv)
+    end
+    val_loss = val_loss / num_val
+    print('val_loss = ', val_loss)
+    table.insert(val_loss_history, val_loss)
+    table.insert(val_loss_history_it, i)
+    model:resetStates()
+    model:training()
+
+    -- First save a JSON checkpoint, excluding the model
+    local checkpoint = {
+      opt = opt,
+      train_loss_history = train_loss_history,
+      val_loss_history = val_loss_history,
+      val_loss_history_it = val_loss_history_it,
+      forward_backward_times = forward_backward_times,
+      memory_usage = memory_usage,
+      i = i
+    }
+    local filename = string.format('%s_%d.json', opt.checkpoint_name, i)
+    -- Make sure the output directory exists before we try to write it
+    paths.mkdir(paths.dirname(filename))
+    utils.write_json(filename, checkpoint)
+
+    -- Now save a torch checkpoint with the model
+    -- Cast the model to float before saving so it can be used on CPU
+    model:clearState()
+    model:float()
+    checkpoint.model = model
+    local filename = string.format('%s_%d.t7', opt.checkpoint_name, i)
+    paths.mkdir(paths.dirname(filename))
+    torch.save(filename, checkpoint)
+    model:type(dtype)
+    params, grad_params = model:getParameters()
+    collectgarbage()
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 -- Train the model!
