461632dc1dadaaa3bb2c2d394a910c59d0e7aac9
LuZhenHuan <603769201@qq.com>
2017-05-26 04:00:34 +0800

    add ems rnn1 2

--- pytorch/EMS_Rnn.py
+++ pytorch/EMS_Rnn.py
@@ -5,9 +5,34 @@
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
+from torch.utils.serialization import load_lua
 
-test_input = Variable(torch.ones(5,2,400))
-test_label = Variable(torch.LongTensor(50))
+N, T ,D= 50, 5, 400	#opt.batch_size, opt.seq_length , word_dim	
+
+train_temp = load_lua('D1Train.t7')
+trainset = train_temp.view(50,-1,2000).transpose(0,1).clone().cuda()
+data_len = trainset.size()[0]
+
+test_temp = load_lua('D1Test.t7')
+testset = train_temp.view(-1,2000).cuda()
+test_len = testset.size()[0]
+
+count = 0
+def read_data():
+    global count, trainset
+
+    x = trainset[count].view(N,T,D).transpose(0,1).clone()
+    x = x.type('torch.FloatTensor')
+
+    count +=  1
+    if(count == data_len):
+        count = 0
+
+    y = torch.LongTensor(50)
+    y[0:25] = 0
+    y[25:50] = 1
+
+    return Variable(x), Variable(y)
 
 ##################################################################
 # build a nerul network 
@@ -34,10 +59,11 @@ class RNN(nn.Module):
         return output, hidden
 
     def initHidden(self):
-        return Variable(torch.zeros(2, self.hidden_size))
+        return Variable(torch.zeros(N, self.hidden_size))
 
 ##################################################################
-# train loop
+# train loop & test loop
+model = RNN(400, 100, 2)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.CrossEntropyLoss()
@@ -45,40 +71,77 @@ criterion = nn.CrossEntropyLoss()
 def train(input,label):
     hidden = model.initHidden()
     optimizer.zero_grad()
+    for i in range(input.size()[0]):
+        output, hidden = model(input[i], hidden)
+    loss = criterion(output, label)
+    loss.backward()
+
+    optimizer.step()
+    return output, loss.data[0]
+
+def test(input):
+    hidden = Variable(torch.zeros(1,100))
+    input = Variable(input.view(5,1,400))
+    input = input.type('torch.FloatTensor')
 
     for i in range(input.size()[0]):
         output, hidden = model(input[i], hidden)
-
-    print(input,input[1],label)    
-    loss = criterion(output, label)
-    loss.backward()
-    print(loss)
-    optimizer.step()
+    top_n, top_i = output.data.topk(1)
+    
+    return top_i[0][0]
 
 ##################################################################
-#let's train it
+# let's train it
 
-model = RNN(400, 100, 2)
+n_epochs = 20
+print_every = data_len
+current_loss = 0
+all_losses = []
+err_rate = []
+err = 0
 
-n_epochs = 5200 
-print_every = 5000
-plot_every = 1000
-current_loss = []
-
-for epoch in range(1, 5200 + 1):
-    target, input = read_data()
+for epoch in range(1, data_len*n_epochs+1):
+    input, target = read_data()
     output, loss = train(input, target)
     current_loss += loss
 
     # Print epoch number, loss, name and guess
     if epoch % print_every == 0:
-        print('%d %d%% (%s) %.4f %s / %s %s' % (epoch, epoch / n_epochs * 100, timeSince(start), loss, line, guess, correct))
+        print(epoch, current_loss / print_every)
+        all_losses.append(current_loss / print_every)
+        current_loss = 0
+        
+        for i in range(test_len):
+            guess = test(testset[i])
+            if i < test_len/2 and guess == 1:
+                err +=1
+            if i >= test_len/2 and guess == 0:
+                err += 1
+        
+        err_rate.append(err/26000)
+        err = 0
+
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+plt.figure()
+plt.plot(all_losses)
+plt.title('loss')
+plt.figure()
+plt.plot(err_rate)
+plt.title('err')
+
+plt.show()
 
 
 
 
 
 
+    
+
+print(err)    
 
 
 
--- pytorch/data_process.py
+++ pytorch/data_process.py
@@ -6,15 +6,15 @@ import torch
 N, T ,D= 50, 5, 400	#opt.batch_size, opt.seq_length , word_dim	
 
 x = load_lua('D1Train.t7')
-x = x.view(50,-1,2000).transpose(0,1)
-#x = x.transpose(0,1)
+x = x.view(50,-1,2000).transpose(0,1).clone()
 
+y = x[1].view(50,5,400).transpose(0,1).clone()
 
-a = x.numpy()
+a = y.numpy()
 
 plt.figure()
-plt.plot(a[9][24])
+plt.plot(a[0][0])
 plt.figure()
-plt.plot(a[9][25])
+plt.plot(a[1][0])
 
 plt.show()
