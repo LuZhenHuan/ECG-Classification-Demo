1d6c4724ce69b21089e6ab04781723815beb44fe
LU <lu@LU-MBP.local>
2018-12-10 17:55:15 +0800

    delet torch files

--- torch/t7Cut.lua
+++
@@ -1,8 +0,0 @@
---[[trainset = torch.load('RnnTrain1D.t7'):view(-1,1)
-
-tempTra =trainset[{{1,40000000},{}}]:view(-1)
-val = trainset[{{40000001,44200000},{}}]:view(-1)
-
-torch.save('RnnTrain1Dcut.t7',tempTra)
-torch.save('RnnVal1Dcut.t7',val)]]--
-
